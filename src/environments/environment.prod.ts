029dbf7c6546bdb04625a24ffb2f035a3151fd67
Maiten Blanc <maitengblanc@gmail.com>
2026-03-13 15:28:53 -0300

    fix: vercel api url in all environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://nest-rick-morty.vercel.app/',
+  baseUrl: 'https://nest-rick-morty.vercel.app',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  baseUrl: 'https://nest-rick-morty.vercel.app/',
+  baseUrl: 'https://nest-rick-morty.vercel.app',
 };
